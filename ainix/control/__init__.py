2a862e3073d4073cfc38fa4ff2f00e98848de0f9
jdnitrap <108910755+jdnitrap@users.noreply.github.com>
2026-08-12 18:19:53 -0400

    Remove ainix files - moved to AINIX repo

--- ainix/control/__init__.py
+++
@@ -1,5 +0,0 @@
-"""AINIX Control System - Safe command execution with guardrails."""
-
-from .executor import SafeExecutor, ExecutionMode, ExecutionResult
-
-__all__ = ['SafeExecutor', 'ExecutionMode', 'ExecutionResult']
